64e7b65b3e18e87758fbcb88cbfbe4cfe8ae0858
Ddfathu <108730817+Ddfathu@users.noreply.github.com>
2026-07-21 11:58:04 +0700

    Manage via Bot

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 PORT=${PORT:-8080}
 
-echo "Memulai Server Wildcard (Accept All UUID/Password)..."
-echo "Railway Port: $PORT"
-
-# Sesuaikan port inbound utama dengan port dinamis Railway
+echo "Memulai Server Wildcard Direct Railway..."
 sed -i "s/8080/$PORT/g" /etc/xray/config.json
 
-# Jalankan Xray
 exec xray -config /etc/xray/config.json
